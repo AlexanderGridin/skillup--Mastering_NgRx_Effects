154c017161cc1cdae47790693ade98133c5b12a3
Alexander Gridin <alexander.m.gridin@gmail.com>
2021-09-08 13:25:38 +0300

    Implement main-navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { StoreModule } from '@ngrx/store';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { postsReducer } from './store/posts/posts.reducer';
 import { JsonPlaceholderService } from './services/json-placeholder/json-placeholder.service';
@@ -11,12 +12,21 @@ import { AppComponent } from './app.component';
 import { PostsListComponent } from './components/posts-list/posts-list.component';
 import { LoaderComponent } from './components/loader/loader.component';
 import { FrontPageComponent } from './pages/front-page/front-page.component';
+import { ROUTES } from './constants/routes';
+import { MainNavigationComponent } from './components/main-navigation/main-navigation.component';
 
 @NgModule({
-  declarations: [AppComponent, PostComponent, PostsListComponent, LoaderComponent, FrontPageComponent],
+  declarations: [
+    AppComponent,
+    PostComponent,
+    PostsListComponent,
+    LoaderComponent,
+    FrontPageComponent,
+    MainNavigationComponent,
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    RouterModule.forRoot(ROUTES),
     HttpClientModule,
     StoreModule.forRoot({ posts: postsReducer }),
   ],
---
+++ src/app/constants/routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+import { FrontPageComponent } from '../pages/front-page/front-page.component';
+
+export const ROUTES: Routes = [
+  {
+    path: '',
+    component: FrontPageComponent,
+  },
+];
